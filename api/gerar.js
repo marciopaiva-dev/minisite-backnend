fc3d3eb20f7f311f386655f4ced292848ecfe819
marciopaiva-dev <marcioldpaiva@gmail.com>
2025-07-18 15:23:10 -0300

    Add files via upload

--- api/gerar.js
+++ api/gerar.js
@@ -1,1 +1,8 @@
-// Função Serverless da Vercel para gerar MiniSite e proposta
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    return res.status(200).send('Mini site gerado (simulado).');
+  } else {
+    return res.status(405).send('Método não permitido');
+  }
+}
